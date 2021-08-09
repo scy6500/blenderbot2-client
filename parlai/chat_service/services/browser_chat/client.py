6d2758fcac0dd1ef160425e64f6b65170d69cea2
scy6500 <55119006+scy6500@users.noreply.github.com>
2021-08-09 16:59:59 +0900

    change https to wss

--- parlai/chat_service/services/browser_chat/client.py
+++ parlai/chat_service/services/browser_chat/client.py
@@ -186,7 +186,7 @@ if __name__ == "__main__":
     port = opt.get('port', 34596)
     print("Connecting to port: ", port)
     ws = websocket.WebSocketApp(
-        "ws://https://master-blenderbot2-server-scy6500.endpoint.ainize.ai/websocket",
+        "wss://master-blenderbot2-server-scy6500.endpoint.ainize.ai/websocket",
         on_message=on_message,
         on_error=on_error,
         on_close=on_close,
